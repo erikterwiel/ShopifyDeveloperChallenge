b46cbcc416071ca94e6f076800436ed9fb6069da
ehterwie <ehterwie@uwaterloo.ca>
2018-09-23 19:38:42 -0400

    Got some structure in

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/shopify");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+const port = "3000";
+app.set("port", port);
+const server = http.createServer(app);
+server.listen(port, () => "Running on port 3000");
