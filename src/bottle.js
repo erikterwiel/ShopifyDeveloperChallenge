f62f7c960e1a9fe5f587cd018973e7cf1eeb743b
ehterwie <ehterwie@uwaterloo.ca>
2018-09-23 20:48:26 -0400

    Configured bottlejs

--- src/bottle.js
+++ src/bottle.js
@@ -1,2 +1,15 @@
 const Bottle = require("bottlejs");
 
+const ShopService = require("./services/Shop");
+const ProductService = require("./services/Product");
+const ProdcutLIService = require("./services/ProductLI");
+const OrderService = require("./services/Order");
+
+const bottle = new Bottle();
+
+bottle.service("shopService", ShopService);
+bottle.service("productService", ProductService);
+bottle.service("productLIService", ProdcutLIService);
+bottle.service("orderService", OrderService);
+
+module.exports = bottle.container;
